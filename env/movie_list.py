28e046df7aadc907d9b93f9f3e3dcd912a8f0b6c
yso <baptisteguillaume5@gmail.com>
2023-02-08 00:13:17 +0100

    commit comment

--- env/movie_list.py
+++ env/movie_list.py
@@ -4,30 +4,40 @@ from bs4 import BeautifulSoup
 def parse_movies_and_export():
     r = requests.get("https://www.imdb.com/chart/top/?ref_=nv_mv_250")
     soup = BeautifulSoup(r.content, "html.parser")
+
+    #we get all the 'tr' tags, globally all the movies
     movies = soup.findAll('tr')
 
-    nom = "Vide"
-    url = "Vide"
-    numero = "Vide"
+    nom = "Empty"
+    url = "Empty"
+    numero = "Empty"
 
+    #i loop on all the movies
     for movie in movies:
+
+        #i get the 'img' tag of the film
         img = movie.find('img')
         if img:
+            #i get the alt property = the name of the film and url property which returns the image
             nom = img.get('alt')
             url = img.get('src')
 
+        #i get the span tag for this film
         span = movie.find('span')
         if span:
+            #i then retrieve its ranking
             numero = span.get('data-value')
 
-        chemin = r"C:\Users\root\Documents\scrap_movie_learning\films.csv"
+        #i configure the path where I want the export
+        path = r"C:\Users\root\Documents\scrap_movie_learning\films.csv"
 
-        with open(chemin, "a", encoding='utf-16-le') as f:
+        #i write for each film, always in the loop
+        with open(path, "a", encoding='utf-16-le') as f:
                 f.write(f"{numero}")
                 f.write(f";")
                 f.write(f"{nom}")
                 f.write(f";")
                 f.write(f"{url}\n")
 
-#we start the treatment
+#i call my function
 parse_movies_and_export()
